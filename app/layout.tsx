d746134459a90a0f1ccdb621b326db36e45b2f49
Pranav Rajveer <rajveer23pr@gmail.com>
2023-09-06 20:41:55 +0530

    added title-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+      <link rel="icon" type="image/svg+xml" href="/public/favicon.png" />
+      </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
